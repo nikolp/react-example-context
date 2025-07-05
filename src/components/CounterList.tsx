dd3f4bcdeb49fd59ddb6c532bece9cf3fe3ebd1f
Philip Nikolov <philip.k.nikolov@gmail.com>
2025-07-05 14:44:49 -0400

    fix unique key

--- src/components/CounterList.tsx
+++ src/components/CounterList.tsx
@@ -2,12 +2,12 @@ import { useCountersContext } from "../store/counters-context";
 import Counter from "./Counter";
 
 export default function CounterList() {
-    // descructure only the piece we need from the context
+    // destructure only the piece we need from the context
     const {counters} = useCountersContext();
     return (
         <ul>
             {counters.map(counter => {
-                return <li key={counter.name}>
+                return <li key={counter.value}>
                     <Counter name={counter.name} value={counter.value} />
                 </li>;
             })}
